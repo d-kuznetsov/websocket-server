c7ac3f2fe3cf5a5ff05e9adedba9467559252e67
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-09-07 14:12:43 +0200

    check if game is over

--- src/game-logic.js
+++ src/game-logic.js
@@ -39,6 +39,16 @@ function handleMove(socket, data) {
     const player = game.players.find((pl) => pl.socket == socket);
     if (game.board[data.row][data.col] === '') {
       game.board[data.row][data.col] = player.symbol;
+      if (game.checkWin()) {
+        game.getCurrentPlayer().gameOver('win', game.board);
+        game.getIdlePlayer().gameOver('lose', game.board);
+        return;
+      }
+      if (game.checkDraw()) {
+        game.getCurrentPlayer().gameOver('draw', game.board);
+        game.getIdlePlayer().gameOver('draw', game.board);
+        return;
+      }
       game.currentPlayer = game.currentPlayer == 'X' ? 'O' : 'X';
       game.update();
     }
@@ -58,9 +68,77 @@ class Game {
   }
 
   update() {
-    this.players.forEach((player) => {
+    const rowFull = this.players.forEach((player) => {
       player.update(this.board, this.currentPlayer);
     });
+  }
+
+  checkWin() {
+    const length = 3;
+    const winRow = this.board.some((row) => {
+      return row.every((col) => col === this.currentPlayer);
+    });
+    if (winRow) {
+      return true;
+    }
+
+    let winCol;
+    for (let colIdx = 0; colIdx < length; colIdx++) {
+      winCol = true;
+      for (let rowIdx = 0; rowIdx < length; rowIdx++) {
+        const symbol = this.board[rowIdx][colIdx];
+        if (symbol !== this.currentPlayer) {
+          winCol = false;
+          break;
+        }
+      }
+      if (winCol) {
+        break;
+      }
+    }
+
+    if (winCol) {
+      return true;
+    }
+
+    let winMainDiagonal = true;
+    for (let idx = 0; idx < length; idx++) {
+      if (this.board[idx][idx] !== this.currentPlayer) {
+        winMainDiagonal = false;
+        break;
+      }
+    }
+    if (winMainDiagonal) {
+      return true;
+    }
+
+    let winSecondDiagonal = true;
+
+    for (
+      let rowIdx = 0, colIdx = length - 1;
+      rowIdx < length;
+      rowIdx++, colIdx--
+    ) {
+      if (this.board[rowIdx][colIdx] !== this.currentPlayer) {
+        winSecondDiagonal = false;
+        break;
+      }
+    }
+    if (winSecondDiagonal) {
+      return true;
+    }
+    return false;
+  }
+
+  checkDraw() {
+    return this.board.every((row) => row.every((col) => col !== ''));
+  }
+
+  getCurrentPlayer() {
+    return this.players.find((pl) => pl.symbol === this.currentPlayer);
+  }
+  getIdlePlayer() {
+    return this.players.find((pl) => pl.symbol !== this.currentPlayer);
   }
 }
 
@@ -89,6 +167,15 @@ class Player {
   }
   opponentDisconnected() {
     this.socket.send(createMessage('oponent Disconnected'));
+  }
+
+  gameOver(result, board) {
+    this.socket.send(
+      createMessage('gameOver', {
+        result,
+        board,
+      })
+    );
   }
 }
 
